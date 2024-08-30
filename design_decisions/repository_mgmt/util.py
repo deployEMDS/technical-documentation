200d09ffd92f2b2106bf3b64d2c0d74a4a104e08
Konstantin <konstantin@headbright.be>
2024-08-30 15:20:11 +0200

    feat: map known links

--- design_decisions/repository_mgmt/util.py
+++ design_decisions/repository_mgmt/util.py
@@ -35,6 +35,7 @@ class TestInformation:
     kpis: Optional[List[TestKpi]] = None
     criteria: Optional[str] = None
 
+
 @dataclass
 class TestResults(TestInformation):
     at_least_one_done: bool = False
@@ -42,7 +43,14 @@ class TestResults(TestInformation):
     num_results_done: int = 0
     results: List[TestResult] = None
 
-    def __init__(self, information: TestInformation, results: List[TestResult], at_least_one_done: bool, num_results: int, num_results_done: int):
+    def __init__(
+        self,
+        information: TestInformation,
+        results: List[TestResult],
+        at_least_one_done: bool,
+        num_results: int,
+        num_results_done: int,
+    ):
         self.id = information.id
         self.title = information.title
         self.description = information.description
@@ -65,9 +73,9 @@ class TestResults(TestInformation):
 
 def _is_result_file_done(file_path):
     """Check if a result file is done by counting TODO entries."""
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         content = f.read()
-        todo_count = content.count('[TODO]')
+        todo_count = content.count("[TODO]")
         return todo_count <= 6  # Consider done if 6 or fewer TODOs remain
 
 
@@ -75,25 +83,61 @@ def read_test_criteria(test_content):
     pattern = r"###.*Comparative[^\n]*\n+(.+?)(?=\n###|$)"
     # pattern = r"###.*criteria[^\n]*\n+(.+?)(?=\n###|$)"
     kpi_criteria = re.search(pattern, test_content, re.DOTALL)
-    kpi_criteria = kpi_criteria.group(1).strip() if kpi_criteria else 'N/A'
+    kpi_criteria = kpi_criteria.group(1).strip() if kpi_criteria else "N/A"
     return kpi_criteria
 
 
+convert_url_map = {
+    "./test.md#comparative-criteria-checklists-": "#Information",
+}
+
+
+def convert_url(link, file_path):
+    """Converts a URL from the original GitHub folder structure to the static web generator structure."""
+    converted_url = link
+    for key, value in convert_url_map.items():
+        if key == link:
+            converted_url = link.replace(key, value)
+            break
+    print("::", link, converted_url)
+    return converted_url
+
+
+def process_urls(content, file_path):
+    """Transforms relative links from the original GitHub folder structures into relative urls compatible with the static web generator."""
+
+    # The pattern matches links starting with './', '../', or '/'
+    pattern = re.compile(r"\[([^\]]+)\]\((\./[^\s)]+|\.\./[^\s)]+|/[^\s)]+)(#[^)]*)?\)")
+
+    def replace_link(match):
+        """Replaces the URL in the matched Markdown link with the resolved path for the static web generator."""
+        text = match.group(1)
+        link = match.group(2)
+        fragment = match.group(3) if match.group(3) else ""
+        upgraded_link = convert_url(link + fragment, file_path)  # Resolve the link
+        return f"[{text}]({upgraded_link})"
+
+    updated_content = re.sub(pattern, replace_link, content)
+    return content
+
 
 def read_test_info(file_path, github_base_url) -> TestInformation:
-    with open(file_path, 'r') as f:
-        test_content = f.read()
-        title_parts = test_content.split('\n')[1].strip('## ').split(' ')
-        title = ' '.join(title_parts[1:])
-        description = re.search(r"### Test description\s*(.*?)(?=\n###|$)", test_content, re.DOTALL)
-        description = description.group(1) if description else 'N/A'
+    with open(file_path, "r") as f:
+        source_content = f.read()
+        test_content = process_urls(source_content, file_path)
+        title_parts = test_content.split("\n")[1].strip("## ").split(" ")
+        title = " ".join(title_parts[1:])
+        description = re.search(
+            r"### Test description\s*(.*?)(?=\n###|$)", test_content, re.DOTALL
+        )
+        description = description.group(1) if description else "N/A"
         test_id_numeric = title_parts[0][1:-1]
-        execution_phase = re.search(r'Phase (\d+)', test_content)
-        execution_phase = execution_phase.group(1) if execution_phase else 'N/A'
-        minimal = 'Minimal?\nYes' in test_content
-        test_type = re.search(r'Test type\n(.+?)\n', test_content)
-        test_type = test_type.group(1) if test_type else 'N/A'
-        dir_of_test = str(file_path).split('tests/')[1]
+        execution_phase = re.search(r"Phase (\d+)", test_content)
+        execution_phase = execution_phase.group(1) if execution_phase else "N/A"
+        minimal = "Minimal?\nYes" in test_content
+        test_type = re.search(r"Test type\n(.+?)\n", test_content)
+        test_type = test_type.group(1) if test_type else "N/A"
+        dir_of_test = str(file_path).split("tests/")[1]
         last_modified = datetime.fromtimestamp(os.path.getmtime(file_path))
 
         return TestInformation(
@@ -106,29 +150,48 @@ def read_test_info(file_path, github_base_url) -> TestInformation:
             last_modified=last_modified,
             path=f"{github_base_url}/{dir_of_test}",
             kpis=read_test_kpis(test_content, test_id_numeric),
-            criteria=read_test_criteria(test_content)
+            criteria=read_test_criteria(test_content),
         )
 
+
 def read_test_kpis(test_content, test_id_numeric) -> List[TestKpi]:
-    kpi_names = re.search(r"#### ISO25010 Quality\s*(.*?)(?=\n####|\n###|$)", test_content, re.DOTALL)
-    kpi_names = (kpi_names.group(1) if kpi_names else '').split('\n')
-    kpi_descs = re.search(r"#### ISO25010 Quality description\s*(.*?)(?=\n####|\n###|$)", test_content, re.DOTALL)
-    kpi_descs = (kpi_descs.group(1) if kpi_descs else '').split('\n')
+    kpi_names = re.search(
+        r"#### ISO25010 Quality\s*(.*?)(?=\n####|\n###|$)", test_content, re.DOTALL
+    )
+    kpi_names = (kpi_names.group(1) if kpi_names else "").split("\n")
+    kpi_descs = re.search(
+        r"#### ISO25010 Quality description\s*(.*?)(?=\n####|\n###|$)",
+        test_content,
+        re.DOTALL,
+    )
+    kpi_descs = (kpi_descs.group(1) if kpi_descs else "").split("\n")
     kpi_names = [name.strip() for name in kpi_names if name.strip()]
     kpi_descs = [desc.strip() for desc in kpi_descs if desc.strip()]
     if len(kpi_names) != len(kpi_descs):
-        print(f'KPI names and descriptions do not match for {test_id_numeric}', kpi_names, kpi_descs)
+        print(
+            f"KPI names and descriptions do not match for {test_id_numeric}",
+            kpi_names,
+            kpi_descs,
+        )
     return [TestKpi(name, desc) for name, desc in zip(kpi_names, kpi_descs)]
 
+
 def read_test_result(file_path, github_base_url) -> TestResult:
-    with open(file_path, 'r') as f:
-        result_content = f.read()
-        stack_groups = re.search(r'Stack: *(.+?)\n', result_content)
-        stack = stack_groups.group(1) if stack_groups else file_path.stem.replace('result_', '')
-        result_groups = re.search(r'Statement of asses*ment\n((.|\n)*)', result_content, re.DOTALL)
-        result = result_groups.group(1) if result_groups else 'N/A'
+    with open(file_path, "r") as f:
+        source_content = f.read()
+        result_content = process_urls(source_content, file_path)
+        stack_groups = re.search(r"Stack: *(.+?)\n", result_content)
+        stack = (
+            stack_groups.group(1)
+            if stack_groups
+            else file_path.stem.replace("result_", "")
+        )
+        result_groups = re.search(
+            r"Statement of asses*ment\n((.|\n)*)", result_content, re.DOTALL
+        )
+        result = result_groups.group(1) if result_groups else "N/A"
         result_done = _is_result_file_done(file_path)
-        dir_of_test_result = str(file_path).split('tests/')[1]
+        dir_of_test_result = str(file_path).split("tests/")[1]
         last_modified = datetime.fromtimestamp(os.path.getmtime(file_path))
 
         return TestResult(
@@ -136,5 +199,5 @@ def read_test_result(file_path, github_base_url) -> TestResult:
             content=result,
             is_done=result_done,
             last_modified=last_modified,
-            path=f"{github_base_url}/{dir_of_test_result}"
+            path=f"{github_base_url}/{dir_of_test_result}",
         )
