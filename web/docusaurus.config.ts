dac645c8dfad78fa53d7c9687f8b6b6fbd54b106
Casper Van Gheluwe <3718739+caspervg@users.noreply.github.com>
2024-09-09 08:34:58 +0200

    ci: Update domain name

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'img/1048_deployEMDS_LOGO-H_RGB.svg',
 
   // Set the production url of your site here
-  url: 'https://salmon-desert-0898cbb03.5.azurestaticapps.net/',
+  url: 'https://deployemds.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
